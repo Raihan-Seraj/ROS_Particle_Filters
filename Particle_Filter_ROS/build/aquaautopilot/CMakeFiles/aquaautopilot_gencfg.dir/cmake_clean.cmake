815707b44bc2c3bc2d1b7f23bac1801ebebd48e7
Raihan Seraj <raihan.seraj@mail.mcgill.ca>
2017-12-17 07:28:46 +0300

    Initial Commit

---
+++ Particle_Filter_ROS/build/aquaautopilot/CMakeFiles/aquaautopilot_gencfg.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/aquaautopilot_gencfg"
+  "/media/raihan/Work/studies/McGill/Intelligent_Robotics/Assignment1/devel/include/aquaautopilot/AutopilotConfig.h"
+  "/media/raihan/Work/studies/McGill/Intelligent_Robotics/Assignment1/devel/share/aquaautopilot/docs/AutopilotConfig.dox"
+  "/media/raihan/Work/studies/McGill/Intelligent_Robotics/Assignment1/devel/share/aquaautopilot/docs/AutopilotConfig-usage.dox"
+  "/media/raihan/Work/studies/McGill/Intelligent_Robotics/Assignment1/devel/lib/python2.7/dist-packages/aquaautopilot/cfg/AutopilotConfig.py"
+  "/media/raihan/Work/studies/McGill/Intelligent_Robotics/Assignment1/devel/share/aquaautopilot/docs/AutopilotConfig.wikidoc"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/aquaautopilot_gencfg.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Particle_Filter_ROS/build/aquaautopilot/CMakeFiles/aquaautopilot_generate_messages_py.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/aquaautopilot_generate_messages_py"
+  "/media/raihan/Work/studies/McGill/Intelligent_Robotics/Assignment1/devel/lib/python2.7/dist-packages/aquaautopilot/msg/_UberpilotStatus.py"
+  "/media/raihan/Work/studies/McGill/Intelligent_Robotics/Assignment1/devel/lib/python2.7/dist-packages/aquaautopilot/msg/__init__.py"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/aquaautopilot_generate_messages_py.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
